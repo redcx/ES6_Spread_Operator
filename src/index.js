0055328c53f9ccc8b14582a39386065f858a844a
HP <37007881+redcx@users.noreply.github.com>
2022-06-03 14:47:17 +0300

    1 1

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", "Banana", "Coconut", ...citrus]
+
+// console.log(fruits);
