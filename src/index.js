e195202e77b0c0b7a68c51024c9b31fad9f2b987
HP <37007881+redcx@users.noreply.github.com>
2022-06-03 14:52:36 +0300

    2 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import App from "./components/App";
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // const citrus = ["Lime", "Lemon", "Orange"];
-// const fruits = ["Apple", "Banana", "Coconut", ...citrus]
+// const fruits = ["Apple", "Banana", "Coconut", ...citrus];
 
-// console.log(fruits);
+// const fullName = {
+//   fName: "James",
+//   lName: "Bond"
+// };
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   username: "jamesbond007"
+// };
+
+// console.log(user);
